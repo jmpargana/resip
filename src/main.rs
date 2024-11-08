f2310a6127167f4c3d72d9606ee9b21f10d459a2
Joao Pargana <jmpargana@gmail.com>
2024-11-08 17:07:39 +0000

    tokio

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,112 @@
 #![allow(unused_imports)]
-use pest::{iterators::Pair, Parser};
-use pest_derive::Parser;
-use std::io::{BufRead, BufReader, Read, Write};
-use std::net::TcpListener;
+use std::io::BufRead;
 
-#[derive(Parser)]
-#[grammar = "resp.pest"]
-pub struct RESPParser;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+    task,
+};
 
-#[derive(Debug, PartialEq)]
-enum ArrayEntry {
-    Int(i32),
-    Text(String),
-    Array(Vec<ArrayEntry>),
-}
+async fn handle_client(mut _stream: TcpStream) {
+    let (reader, mut writer) = _stream.into_split();
+    let mut reader = BufReader::new(reader);
 
-fn extract_string_value(pair: Pair<Rule>) -> &str {
-    pair.into_inner()
-        .find(|p| p.as_rule() == Rule::text)
-        .expect("Expected at least one string")
-        .as_str()
-}
+    loop {
+        let mut buffer = Vec::new();
 
-// Helper function to extract the integer from a `Pair` for `int`
-fn extract_int_value(pair: Pair<Rule>) -> i32 {
-    pair.into_inner()
-        .next()
-        .expect("Expected number after ':'")
-        .as_str()
-        .parse::<i32>()
-        .expect("failed to parse number")
-}
+        match reader.read_until(b'\n', &mut buffer).await {
+            Ok(0) => {
+                println!("client disconnected");
+                break;
+            }
+            Ok(_) => {
+                let input = String::from_utf8_lossy(&buffer);
 
-fn extract_array_entries(pair: Pair<Rule>) -> Vec<ArrayEntry> {
-    pair.into_inner()
-        .filter_map(|p| match p.as_rule() {
-            Rule::int => Some(ArrayEntry::Int(extract_int_value(p))),
-            Rule::string => Some(ArrayEntry::Text(extract_string_value(p).to_string())),
-            Rule::array => Some(ArrayEntry::Array(extract_array_entries(p))),
-            _ => None,
-        })
-        .collect()
-}
-
-fn main() {
-    println!("Logs from your program will appear here!");
-
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                let mut reader =
-                    BufReader::new(_stream.try_clone().expect("failed to copy stream"));
-                let mut writer = _stream;
-
-                loop {
-                    let mut buffer = Vec::new();
-
-                    match reader.read_until(b'\n', &mut buffer) {
-                        Ok(0) => {
-                            break;
-                        }
-                        Ok(_) => {
-                            let input = String::from_utf8_lossy(&buffer);
-
-                            if input.trim() == "PING" {
-                                writer.write_all(b"+PONG\r\n").expect("failed to write");
-                                writer.flush().expect("failed flushing");
-                            }
-                        }
-                        Err(e) => {
-                            println!("failed reading, {}", e);
-                            break;
-                        }
-                    }
+                if input.trim() == "PING" {
+                    let _ = writer.write_all(b"+PONG\r\n").await;
                 }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("failed reading, {}", e);
+                break;
             }
         }
     }
 }
 
+#[tokio::main]
+async fn main() {
+    println!("Logs from your program will appear here!");
+
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .expect("failed to bind");
+
+    loop {
+        let (stream, _) = listener.accept().await.expect("failed to accept listener");
+        task::spawn(async move {
+            handle_client(stream).await;
+        });
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::{BufRead, BufReader, Read, Write};
+    use std::net::TcpStream;
+    use std::thread;
+    use tokio::time::{sleep, Duration};
+
     use super::*;
 
-    #[test]
-    fn can_parse_int() {
-        let input = ":123\r\n";
-        let expected = 123;
-        let actual = RESPParser::parse(Rule::int, input)
-            .expect("failed to parse input")
-            .next()
-            .expect("expected a single pair");
+    #[tokio::test]
+    async fn test_connection_is_accepted() {
+        thread::spawn(|| {
+            main(); // Start server in separate thread
+        });
 
-        assert_eq!(extract_int_value(actual), expected);
+        sleep(Duration::from_millis(100)).await;
+
+        let client = TcpStream::connect("127.0.0.1:6379");
+        assert!(client.is_ok(), "Expected connection to be accepted");
     }
 
-    #[test]
-    fn can_parse_string() {
-        let input = "$3\r\nhey\r\n";
-        let expected = "hey";
-        let actual = RESPParser::parse(Rule::string, input)
-            .expect("failed to parse input")
-            .next()
-            .expect("expected a single pair");
+    #[tokio::test]
+    async fn test_if_ping_gets_pong() {
+        thread::spawn(|| {
+            main(); // Start server in separate thread
+        });
 
-        assert_eq!(extract_string_value(actual), expected);
+        sleep(Duration::from_millis(100)).await;
+
+        let mut client = TcpStream::connect("127.0.0.1:6379").expect("failed to connect");
+        client.write_all(b"PING\r\n").expect("failed to write");
+
+        let mut reader = BufReader::new(client);
+        let mut response = String::new();
+        reader.read_line(&mut response).expect("Failed to read");
+
+        assert_eq!(response, "+PONG\r\n");
     }
 
-    #[test]
-    fn can_parse_array() {
-        let input = "*2\r\n:2\r\n$5\r\nthree\r\n";
-        let expected = vec![ArrayEntry::Int(2), ArrayEntry::Text("three".to_string())];
-        let actual = RESPParser::parse(Rule::array, input)
-            .expect("failed to parse input")
-            .next()
-            .expect("expected a single pair");
+    #[tokio::test]
+    async fn test_if_multiple_pings_get_pongs() {
+        thread::spawn(|| {
+            main(); // Start server in separate thread
+        });
 
-        let entries = extract_array_entries(actual);
+        sleep(Duration::from_millis(100)).await;
 
-        assert_eq!(entries, expected);
-    }
+        let mut client = TcpStream::connect("127.0.0.1:6379").expect("failed to connect");
 
-    #[test]
-    fn can_parse_array_recursively() {
-        let input = "*2\r\n:2\r\n$5\r\nthree\r\n*1\r\n:4\r\n";
-        let expected = vec![
-            ArrayEntry::Int(2),
-            ArrayEntry::Text("three".to_string()),
-            ArrayEntry::Array(vec![ArrayEntry::Int(4)]),
-        ];
-        let actual = RESPParser::parse(Rule::array, input)
-            .expect("failed to parse input")
-            .next()
-            .expect("expected a single pair");
+        for _ in 0..3 {
+            client.write_all(b"PING\r\n").expect("Failed to write");
 
-        let entries = extract_array_entries(actual);
+            let mut reader = BufReader::new(client.try_clone().expect("Failed to clone client"));
+            let mut response = String::new();
+            reader.read_line(&mut response).expect("Failed to read");
 
-        assert_eq!(entries, expected);
+            assert_eq!(response.trim(), "+PONG", "Expected response to be +PONG");
+        }
     }
 }
